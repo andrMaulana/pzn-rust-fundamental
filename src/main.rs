cd72901cbd38d610dce986f30085e088199933e7
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-07 21:28:44 +0700

    18.2 fiks Loop Return Value

--- src/main.rs
+++ src/main.rs
@@ -401,3 +401,17 @@ fn loop_expression() {
         println!("{}", number);
     }
 }
+
+// Loop Return Value
+#[test]
+fn loop_return_value() {
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter > 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("{}", result);
+}
